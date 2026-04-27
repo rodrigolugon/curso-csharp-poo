3155a5eda716c3279ad5ce7d6edf92a77c524217
Rodrigo Lugon <rodrigolugonn@gmail.com>
2026-04-27 11:08:06 -0300

    feat: add string split exercise using strtok

--- Aula08_Exercicio5/Program.cs
+++ Aula08_Exercicio5/Program.cs
@@ -1,15 +1,22 @@
-﻿namespace Aula08_Exercicio5
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            int quantityItem1, quantityItem2, codeItem1, codeItem2;
+﻿namespace Aula08_Exercicio5 {
+    internal class Program {
+        static void Main(string[] args) {
+            int codeItem1, codeItem2, quantityItem1, quantityItem2;
             double priceItem1, priceItem2, totalPrice;
 
-            Console.WriteLine("-----STORE-----");
-            Console.WriteLine("Digite o codigo do item1: ");
-            codeItem1 = int.Parse(Console.ReadLine());
+            string[] item1 = Console.ReadLine().Split(' ');
+            codeItem1 = int.Parse(item1[0]);
+            quantityItem1 = int.Parse(item1[1]);
+            priceItem1 = double.Parse(item1[2]);
+
+            string[] item2 = Console.ReadLine().Split(' ');
+            codeItem2 = int.Parse(item2[0]);
+            quantityItem2 = int.Parse(item2[1]);
+            priceItem2 = double.Parse(item2[2]);
+
+            totalPrice = (quantityItem1 * priceItem1) + (quantityItem2 * priceItem2);
+
+            Console.WriteLine($"VALOR A PAGAR: R$ {totalPrice:F2}");
         }
     }
 }
