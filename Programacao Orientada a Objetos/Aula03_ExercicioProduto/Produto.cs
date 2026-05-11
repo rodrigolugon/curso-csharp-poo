99586c2387e799ea1b2ab2909d50feea70ced117
Rodrigo Lugon <rodrigolugonn@gmail.com>
2026-05-11 11:45:22 -0300

    exercicio produto, criacao da classe e metodo da quantidade do estoque

---
+++ Programacao Orientada a Objetos/Aula03_ExercicioProduto/Produto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula03_ExercicioProduto {
+    internal class Produto {
+
+        //nome de atributo e classe sao c/ letra MAIUSCULA
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEmEstoque() {
+            return Preco * Quantidade;
+        }
+
+
+    }
+}
---
+++ Programacao Orientada a Objetos/Aula03_ExercicioProduto/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+namespace Aula03_ExercicioProduto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto produto1 = new Produto(); //intanciacao
+
+            Console.WriteLine("Entre com os dados do produto: ");
+            Console.Write("Nome: ");
+            produto1.Nome = Console.ReadLine();
+            Console.Write("Preco: ");
+            produto1.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade no estoque: ");
+            produto1.Quantidade = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Dados do produto: ");
+
+        }
+    }
+}
